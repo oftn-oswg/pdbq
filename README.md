aff0036f632b86c985e52a81963d282c168107f6
Devin Samarin <eboyjr14@gmail.com>
2013-12-11 03:03:48 -0800

    Put in a simple rotating triangle; use GLEW

--- README.md
+++ README.md
@@ -8,11 +8,19 @@ The ultimate game modification playground.
 # Install required development files
 sudo apt-get install xorg-dev libglu1-mesa-dev
 
-# Download, extract, and compile GLFW
+# Download, extract, compile, and install GLFW 3.0.3
 wget https://github.com/glfw/glfw/archive/3.0.3.zip && \
   unzip 3.0.3.zip && cd glfw-3.0.3 && mkdir build && cd build && \
-  cmake .. && make
-  
-# Install GLFW
-sudo make install
+  cmake .. && make && sudo make install
+
+# Download, extract, compile, and install GLEW 1.10.0
+wget \
+  http://downloads.sourceforge.net/project/glew/glew/1.10.0/glew-1.10.0.tgz && \
+  tar -xf glew-1.10.0.tgz && cd glew-1.10.0 && make && sudo make install
+```
+
+## Building & Running
+
+```sh
+cd src && make && ./main
 ```
--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-static void error(int error, const char *desc) {
-	fprintf (stderr, "glfw: (#%d) %s\n", error, desc);
+static void error (int error, const char *desc) {
+	fprintf (stderr, "GLFW: %s\n", desc);
 }
 
-int main(void) {
+int main (void) {
+	GLenum err;
 	GLFWwindow *window;
 
 	glfwSetErrorCallback (error);
@@ -16,16 +18,55 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 1);
+	// Context profiles only exist for OpenGL version 3.2 and above.
+	// glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	window = glfwCreateWindow (640, 480, "Untitled Window", NULL, NULL);
 	if (!window) {
 		glfwTerminate ();
 		return EXIT_FAILURE;
 	}
 
-	glfwMakeContextCurrent(window);
+	err = glewInit ();
+	if (err != GLEW_OK) {
+		fprintf (stderr, "GLEW: %s\n", glewGetErrorString(err));
+		glfwTerminate ();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent (window);
 
 	while (!glfwWindowShouldClose (window)) {
-		/* Render here */
+		/* Begin rendering code */
+		float ratio;
+		int width, height;
+
+		glfwGetFramebufferSize (window, &width, &height);
+		ratio = width / (float) height;
+
+		glViewport (0, 0, width, height);
+		glClearColor (1.f, 1.f, 1.f, 1.f);
+		glClear (GL_COLOR_BUFFER_BIT);
+
+		glMatrixMode (GL_PROJECTION);
+		glLoadIdentity ();
+		glOrtho (-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		glMatrixMode (GL_MODELVIEW);
+
+		glLoadIdentity ();
+		glRotatef ((float) glfwGetTime () * 50.f, 0.f, 0.f, 1.f);
+
+		glBegin (GL_TRIANGLES);
+		glColor3f (1.f, 0.f, 0.f);
+		glVertex3f (-0.6f, -0.4f, 0.f);
+		glColor3f (0.f, 1.f, 0.f);
+		glVertex3f (0.6f, -0.4f, 0.f);
+		glColor3f (0.f, 0.f, 1.f);
+		glVertex3f (0.f, 0.6f, 0.f);
+		glEnd ();
+		/* End rendering code */
 
 		glfwSwapBuffers (window);
 		glfwPollEvents ();
